e4ea92c488a7ce776c9c5dba98b7fa2333693908
David Levy <lennox.davidlevy@gmail.com>
2020-12-27 21:57:20 -0500

    login styled, prepared for api

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,78 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const Login = () => {
-  const initialState = { user: '', password: '' };
-  const [form, formData] = useState(initialState);
-  const handleClickGet = async () => {
-    try {
-      const res = await axios.get('/login');
-      console.log(res);
-    } catch (err) {
-      console.error(err);
-    }
+const Login = ({ handleMouseDown, handleMouseUp }) => {
+  const initialState = { email: '', password: '' };
+  const [formData, setFormData] = useState(initialState);
+  const [showForm, setShowForm] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //send post to server
+    const resetLogin = document.getElementById('login-nav');
+    resetLogin.classList.remove('clicked');
+    setShowForm(false);
+    setFormData(initialState);
   };
 
-  const handleClickPost = async () => {
-    try {
-      const res = await axios.post('/login');
-      console.log(res);
-    } catch (err) {
-      console.error(err);
-    }
+  const handleCancel = () => {
+    const resetLogin = document.getElementById('login-nav');
+    resetLogin.classList.remove('clicked');
+    setShowForm(false);
   };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((oldFormData) => {
+      return { ...oldFormData, [name]: value };
+    });
+  };
+
   return (
     <div>
-      <button onClick={handleClickGet}>Get</button>
-      <button onClick={handleClickPost}>Post</button>
+      <div
+        id="login-nav"
+        className="login-nav"
+        onMouseDown={(e) => handleMouseDown(e)}
+        onClick={() => setShowForm(true)}
+      >
+        Login
+      </div>
+
+      {showForm && (
+        <div className="login-form">
+          <form className="login-container">
+            <input
+              name="email"
+              value={formData.email}
+              type="email"
+              onChange={(e) => handleChange(e)}
+              placeholder="Enter Email..."
+            />
+            <input
+              name="password"
+              value={formData.password}
+              type="password"
+              onChange={(e) => handleChange(e)}
+              placeholder="Enter Password..."
+            />
+            <div className="login-button-group">
+              <button
+                className="submit-button"
+                onClick={(e) => handleSubmit(e)}
+                disabled={
+                  formData.email.length === 0 || formData.password.length === 0
+                }
+              >
+                Submit
+              </button>
+              <button className="cancel-button" onClick={() => handleCancel()}>
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
