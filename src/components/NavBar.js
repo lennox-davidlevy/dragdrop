2a94b6a9357a7d5f743871288652100ac00546fc
David Levy <lennox.davidlevy@gmail.com>
2021-01-02 13:26:06 -0500

    added time to taskbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,14 @@
-import React, { useContext } from 'react';
-import Account from './Account';
-import Login from './Login';
+import React from 'react';
+
 import StartMenu from './StartMenu';
-import { UserContext } from './UserContext';
-import windowsIcon from '../img/windowsIcon.png';
+import Clock from './Clock';
 
-const NavBar = ({ numberOfGroups, setShowErrorMessage, setErrorMessages }) => {
-  const handleMouseDown = (e) => {
-    e.target.classList.add('clicked');
-  };
-  const handleMouseUp = (e) => {
-    e.target.classList.remove('clicked');
-  };
-  const { user } = useContext(UserContext);
-
+const NavBar = () => {
   return (
     <div className="taskBar">
-      {/* <div
-        className="start-nav"
-        // onMouseDown={(e) => handleMouseDown(e)}
-        // onMouseUp={(e) => handleMouseUp(e)}
-      >
-        <img className="windows-icon" src={windowsIcon} />
-        <span>Board</span>
-      </div> */}
       <StartMenu />
       <div className="right-side-nav">
-        {/* {!user ? (
-          <Login
-            handleMouseDown={handleMouseDown}
-            handleMouseUp={handleMouseUp}
-            setShowErrorMessage={setShowErrorMessage}
-            setErrorMessages={setErrorMessages}
-          />
-        ) : (
-          <Account
-            handleMouseDown={handleMouseDown}
-            handleMouseUp={handleMouseUp}
-          />
-        )} */}
-
-        {numberOfGroups === 1 ? (
-          <div className="group-num-nav">{numberOfGroups} Group</div>
-        ) : (
-          <div className="group-num-nav">{numberOfGroups} Groups</div>
-        )}
+        <Clock />
       </div>
     </div>
   );
