21fc9b93d01ad2afde5438e8e1455c68b7785303
David Levy <lennox.davidlevy@gmail.com>
2021-01-03 21:46:08 -0500

    pre deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('../config/db');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 connectDB();
@@ -10,14 +11,14 @@ const app = express();
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('API Running');
-});
-
 app.use('/user', require('./routes/user'));
 app.use('/randomWord', require('./routes/randomWord'));
 app.use('/auth', require('./routes/auth'));
 app.use('/boards', require('./routes/boards'));
+
+// if(process.env.NODE_ENV === 'production'){
+//   app.use()
+// }
 app.listen(PORT, () => {
   console.log(`server started on ${PORT}...`);
 });
